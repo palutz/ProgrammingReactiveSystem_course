60301f2be90fb4600c380e5a26f4daf2fed4adf0
palutz <stefano.paluello@gmail.com>
2019-03-10 01:02:44 +0000

    02.02 counter

--- 02_TheActorModel/lessons/src/main/scala/counter.scala
+++ 02_TheActorModel/lessons/src/main/scala/counter.scala
@@ -1,29 +1,44 @@
-package conm.example
+package a.lesson02
 
 import akka.actor.{Actor, Props, ActorSystem }
 
+/**
+Example code like in the lesson 02.02 (with some modifications)
+
+  */
+
 class Counter extends Actor {
-  def counter(n: Int): Receive = {
-    case "incr" => context.become(counter(n + 1))
-    case "get"  => sender ! n
+  def counter(n : Int) : Receive = {
+    case "inc" => {
+      println("n=" + n)
+      context.become(counter(n + 1))
+    }
+    case "get" => sender ! n
   }
   def receive = counter(0)
 }
 
-import akka.actor.Actor
-import akka.actor.Props
-
 class CounterMain extends Actor {
   val counter = context.actorOf(Props[Counter], "counter")
-
-  counter ! "incr"
-  counter ! "incr"
-  counter ! "incr"
-  counter ! "get"
-
   def receive = {
-    case count: Int =>
-      println(s"count was $count")
-      context.stop(self)
+    case n : Int => println("received " + n)
+    case "add" => counter ! "inc"
+    case "tot" => counter ! "get"
   }
+}
+
+object IntroToActor extends App {
+  println("Starting...")
+  val ctx = ActorSystem("helloAkka")
+  val cm = ctx.actorOf(Props[CounterMain], "CounterMain")
+  println("sending msg to main actor")
+  cm ! 0
+  cm ! "add"
+  cm ! "add"
+  cm ! "add"
+  println("going to sleep....")
+  Thread.sleep(1000)
+  cm ! "tot"
+  println("terminating...")
+  ctx.terminate()
 }
