d9f95623ef64d00360cdce5309a80f321d587eae
palutz <stefano.paluello@gmail.com>
2019-03-16 18:41:40 +0000

    counter2, more reactive way

--- 02_TheActorModel/lessons/src/main/scala/counter.scala
+++ 02_TheActorModel/lessons/src/main/scala/counter.scala
@@ -41,24 +41,45 @@ class CounterMain extends Actor {
   }
 }
 
+// making all the code more reactive
+// this actor will just send the msgs to the counter
+// and it will receive the answer in the receive msg (an msg with an int, the tot)
+class CounterMain2 extends Actor {
+  val counter = context.actorOf(Props[Counter], "counter")
+
+  counter ! "inc"
+  counter ! "inc"
+  counter ! "inc"
+
+  counter ! "get"
+
+
+  // print out what will be received as a msg 
+  def receive = {
+    case count : Int =>
+      println(s"the result is $count")
+  }
+
+}
+
 object IntroToActor extends App {
   println("Starting...")
   val ctx = ActorSystem("helloAkka")
-  val cm = ctx.actorOf(Props[CounterMain], "CounterMain")
-  println("sleep after creating CounterMain...")
-  Thread.sleep(1000)
-  println("sending msg to main actor")
-  cm ! "add"
-  cm ! "add"
-  cm ! "add"
-  cm ! "tot"
-  println("going to sleep....")
-  Thread.sleep(1000)
-  println("terminating...")
+  val cm = ctx.actorOf(Props[CounterMain2], "CounterMain")
+  // println("sleep after creating CounterMain...")
+  // Thread.sleep(1000)
+  // println("sending msg to main actor")
+  // cm ! "add"
+  // cm ! "add"
+  // cm ! "add"
+  // cm ! "tot"
+  // println("going to sleep....")
+  // Thread.sleep(1000)
+  // println("terminating...")
   ctx.terminate()
 }
 
-/* Output in this configuration:
+/* Output in the first configuration (CounterMain)
  Starting...
  sleep after creating CounterMain...
  CounterMain receive
@@ -74,4 +95,13 @@ object IntroToActor extends App {
  n=2
  CounterMain received 3
  terminating...
+ */
+
+/* Output in the second configuration (CounterMain2)
+ Starting...
+ receive
+ n=0
+ n=1
+ n=2
+ the result is 3
  */
