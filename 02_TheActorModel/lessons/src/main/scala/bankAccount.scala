d206e7915deef1b24eb252dd4b8adc318dfd7c4d
palutz <stefano.paluello@gmail.com>
2019-03-17 01:29:09 +0000

    examples

--- 02_TheActorModel/lessons/src/main/scala/bankAccount.scala
+++ 02_TheActorModel/lessons/src/main/scala/bankAccount.scala
@@ -1,6 +1,7 @@
 package a.lesson02
 
 import akka.actor.{Actor, Props, ActorSystem, ActorRef }
+import akka.event.LoggingReceive
 
 object BankAccount {
   case class Deposit(amount : BigInt) {
@@ -18,16 +19,15 @@ object BankAccount {
 class BankAccount extends Actor {
   import BankAccount._
 
-  def inner(balance: BigInt) : Receive = {
-    case Deposit(a) => {
+  def inner(balance: BigInt) : Receive = LoggingReceive {
+    case Deposit(a) =>
+      sender ! Done
       context.become(inner(balance + a))
+    case Withdraw(a) if(balance >= a) =>
       sender ! Done
-    }
-    case Withdraw(a) if(balance >= a) => {
       context.become(inner(a))
-      sender ! Done
-    }
-    case _ => sender ! Failed
+    case _ =>
+      sender ! Failed
   }
 
   def receive = inner(0)
@@ -48,7 +48,7 @@ class WireTransfer extends Actor {
       context.become(awaitWithdraw(to, amount, sender))  // await for confirmation of withdraw (actor like suspends itself)
   }
 
-  def awaitWithdraw(to: ActorRef, amount: BigInt, client: ActorRef) : Receive = {
+  def awaitWithdraw(to: ActorRef, amount: BigInt, client: ActorRef) : Receive = LoggingReceive {
     case BankAccount.Done =>
       to ! BankAccount.Deposit(amount)
       context.become(awaitDeposit(client))
@@ -57,7 +57,7 @@ class WireTransfer extends Actor {
       context.stop(self)
   }
 
-  def awaitDeposit(client: ActorRef) : Receive = {
+  def awaitDeposit(client: ActorRef) : Receive =  LoggingReceive {
     case BankAccount.Done =>
       client ! Done  // confirm the success to the original client
       context.stop(self)
@@ -70,18 +70,25 @@ class TransferMain extends Actor {
 
   accA ! BankAccount.Deposit(100)
 
-  def receive = {
-    case BankAccount.Done => transfer(50)
+  def receive = LoggingReceive {
+    case BankAccount.Done =>
+      println("deposit done.. tranferring")
+      transfer(50)
+    case BankAccount.Failed =>
+      println("BA failed")
   }
 
   def transfer(a : BigInt) : Unit = {
     val t = context.actorOf(Props[WireTransfer], "transfer")
     t ! WireTransfer.Transfer(accA, accB, a)
-    context.become {
+    context.become(LoggingReceive{
       case WireTransfer.Done =>
-        println("Done")
+        println("Success transfer")
         context.stop(self)
-    }
+      case WireTransfer.Failed =>
+        println("Failed")
+        context.stop(self)
+    })
   }
 }
 
