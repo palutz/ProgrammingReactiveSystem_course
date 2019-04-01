6b8cb605b84c1e8b7c4f6526f7874b6b9f250e9b
palutz <stefano.paluello@gmail.com>
2019-04-01 07:22:21 +0100

    receptionist fixed

--- 02_TheActorModel/02_Code/myLinkChecker/src/main/scala/Receptionist.scala
+++ 02_TheActorModel/02_Code/myLinkChecker/src/main/scala/Receptionist.scala
@@ -1,55 +1,65 @@
 package it.stefano.linkChecker
 
-import akka.actor.{Actor, ActorRef, ActorLogging, Props }
+import akka.actor.{Actor, ActorRef, ActorLogging, Props, SupervisorStrategy }
 
 /****************************
  Manage one contoller for
  one request
  * ************************/
-class Receptionist extends Actor {
-  def receive = waiting
-  val waiting: Receive = {
-    // upon Get(url) start a traversal and become running
-  }
-  def running(queue: Vector[Job]): Receive = {
-    // upon Get(url) apppend that to queue and keep running
-    // upon Controller.Result(links) ship that to client
-    // and run next job from queue (if any)
-  }
-
+object Receptionist {
+  private case class Job(client: ActorRef, url: String)
+  case class Get(url: String)
+  case class Result(url: String, links: Set[String])
+  case class Failed(url: String, reason: String)
+  case class Terminated(msg: String)
 }
 
+class Receptionist extends Actor {
+  import Receptionist._
 
- // create an actor to simulate a queue
-case class Job(client: ActorRef, url: String) extends Actor {
+  def controllerProps: Props = Props[Controller]
+
+  override def supervisorStrategy = SupervisorStrategy.stoppingStrategy
+
   var reqNo = 0
+
+  def receive = waiting
+
+  val waiting: Receive = {
+    case Get(url) =>
+      context.become(runNext(Vector(Job(sender, url))))
+  }
+
+  def running(queue: Vector[Job]): Receive = {
+    case Controller.Result(links) =>
+      val job = queue.head
+      job.client ! Result(job.url, links)
+      context.stop(context.unwatch(sender))
+      context.become(runNext(queue.tail))
+    case Terminated(_) =>
+      val job = queue.head
+      job.client ! Failed(job.url, "controller failed unexpectedly")
+      context.become(runNext(queue.tail))
+    case Get(url) =>
+      context.become(enqueueJob(queue, Job(sender, url)))
+  }
 
   def runNext(queue: Vector[Job]): Receive = {
     reqNo += 1
     if (queue.isEmpty) waiting
     else {
-      val controller = context.actorOf(Props[Controller], s"c$reqNo") //reqNo - unique number for every request 
+      val controller = context.actorOf(controllerProps, s"c$reqNo")
+      context.watch(controller)
       controller ! Controller.Check(queue.head.url, 2)
       running(queue)
     }
   }
+
   def enqueueJob(queue: Vector[Job], job: Job): Receive = {
     if (queue.size > 3) {
-      sender ! Failed(job.url)
+      sender ! Failed(job.url, "queue overflow")
       running(queue)
     } else running(queue :+ job)
   }
 
-  val waiting: Receive = {
-    case Get(url) => context.become(runNext(Vector(Job(sender, url))))
-  }
-  def running(queue: Vector[Job]): Receive = {
-    case Controller.Result(links) =>
-      val job = queue.head
-      job.client ! Result(job.url, links)
-      context.stop(sender)
-      context.become(runNext(queue.tail))
-    case Get(url) =>
-      context.become(enqueueJob(queue, Job(sender, url)))
-  }
 }
