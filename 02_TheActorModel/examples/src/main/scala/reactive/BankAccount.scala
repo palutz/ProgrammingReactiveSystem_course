e0e1e415d0aa7b355f8c09ff7e7e6102bff12253
palutz <stefano.paluello@gmail.com>
2019-05-03 20:44:57 +0100

    bank account reactive, immutable

--- 02_TheActorModel/examples/src/main/scala/reactive/BankAccount.scala
+++ 02_TheActorModel/examples/src/main/scala/reactive/BankAccount.scala
@@ -1,6 +1,6 @@
 package reactive
 
-import akka.actor.Actor
+import akka.actor.{ Actor, ActorSystem, Props }
 import akka.event.LoggingReceive
 
 object BankAccount {
@@ -10,20 +10,51 @@ object BankAccount {
   case class Withdraw(amount: BigInt) {
     require(amount > 0)
   }
+  case object Statement
   case object Done
   case object Failed
 }
 
 class BankAccount extends Actor {
   import BankAccount._
-  var balance = BigInt(0)
-  def receive = LoggingReceive {
+  // var balance = BigInt(0)
+
+  def bankAccount(money: BigInt) : Receive = {
     case Deposit(amount) =>
-      balance += amount
-      sender ! Done
-    case Withdraw(amount) if amount <= balance =>
-      balance -= amount
-      sender ! Done
+      // balance += amount
+      println(s"deposit $amount")
+      // sender ! Done
+      context.become(bankAccount(money + amount))
+    case Withdraw(amount) if amount <= money =>
+      // balance -= amount
+      println(s"withdraw $amount")
+      // sender ! Done
+      context.become(bankAccount(money - amount))
+    case Statement =>
+      println("Bank account statement: ")
+      // sender ! Done
+      println(s"money= $money")
     case _ => sender ! Failed
   }
+
+  def receive = LoggingReceive {
+    println("starting receive")
+    bankAccount(0)
+  }
+}
+
+object TestBankActor extends App {
+  println("start bank test")
+  val ctx = ActorSystem("SteoAkka")
+  val bA = ctx.actorOf(Props[BankAccount], "steoBank")
+
+  bA ! BankAccount.Deposit(100)
+  bA ! BankAccount.Deposit(100)
+  bA ! BankAccount.Deposit(100)
+
+  bA ! BankAccount.Withdraw(150)
+
+  bA ! BankAccount.Statement
+
+  ctx.terminate()
 }
